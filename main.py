3e6f8c9d9823cdacb229170079ac2cafb68bc859
andihasan97 <andihasanelfalakiy@gmail.com>
2022-09-11 17:41:24 +0700

    First Push luas-volume-python

---
+++ main.py
@@ -0,0 +1,12 @@
+import moduleku as math
+
+input = float(input("Masukkan Diameter (cm) untuk menghitung Luas Lingkaran & Volume Bola: "))
+
+hasil = round(math.circle(input), 2)
+
+volume = round(math.ball(input), 2)
+print(" ")
+print(f"Luas Lingkaran: {hasil} cm²")
+
+print(" ")
+print(f"Volume Bola: {volume} cm³")
---
+++ moduleku.py
@@ -0,0 +1,9 @@
+phi = 3.1415926535897931
+
+def circle(d):
+    total_luas = phi * (d/2) ** 2
+    return total_luas
+    
+def ball(d):
+    total_volume = ((d/2) ** 3 * 4.0 / 3.0) * phi
+    return total_volume
